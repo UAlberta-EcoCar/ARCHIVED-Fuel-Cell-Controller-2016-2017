1cd114d366702492528461149338ff846e12f3c0
egierl <egierl@ualberta.ca>
2017-03-25 13:37:09 -0600

    cleaning up the code

--- Firmware/CVM/CVM code/CVM/CVM.ino
+++ Firmware/CVM/CVM code/CVM/CVM.ino
@@ -5,8 +5,8 @@
 
 
  
-int sigOne = A3;                // sig 1 connected to FCC ethernet pin 5, is this right?
-int sigTwo = A2;                // sig 1 connected to FCC ethernet pin 6, is this right?
+int sigOne = A3;                // sig 1 connected to FCC ethernet pin 5
+int sigTwo = A2;                // sig 1 connected to FCC ethernet pin 6
 
 //Variable for storing voltage readings
 float CellVoltages[46];
@@ -15,12 +15,14 @@ int CellN = 0;
 // holds average voltage value
 int AverageAdcVoltage=0;
 // i2c variables
- uint8_t ack = 0;
- uint16_t adc_code = 0;
+uint8_t ack = 0;
+uint16_t adc_code = 0;
+uint8_t adc_command =0;
+float adc_voltage= 0;
 
 uint16_t Channels[8] = {0x80,0xC0,0x90,0xD0,0xA0,0xE0,0xB0,0xF0}; //confirm int not char***
-//uint16_t chipAddress[6] = {LTC2309_I2C_ADDRESS_1,LTC2309_I2C_ADDRESS_2,LTC2309_I2C_ADDRESS_3,LTC2309_I2C_ADDRESS_4,LTC2309_I2C_ADDRESS_5,LTC2309_I2C_ADDRESS_6}; 
-int arrayAdcVoltage[20]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+uint16_t chipAddress[6] = {LTC2309_I2C_ADDRESS_1,LTC2309_I2C_ADDRESS_2,LTC2309_I2C_ADDRESS_3,LTC2309_I2C_ADDRESS_4,LTC2309_I2C_ADDRESS_5,LTC2309_I2C_ADDRESS_6}; 
+float arrayAdcVoltage[20]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
 
 void setup() {
@@ -31,26 +33,25 @@ pinMode(sigTwo, OUTPUT); // sets the digital pin as output
 digitalWrite(sigOne, LOW);  // turn off signal
 digitalWrite(sigTwo, LOW);  // turn off signal
 }
-char p = 0;
+//char p = 0;
 
 void loop() 
-{
- 
-   
+{  
 collectData(); //call function collectData
 
   //Calculate voltage of each cell relative to the one before it
   for(char CellN=45;CellN>0;CellN--)
    {
    CellVoltages[CellN] = CellVoltages[CellN] - CellVoltages[CellN-1];
-       //send signal to show that pin is disconnected 
-               if (CellVoltages[CellN] <0.1)
-                {
-                    digitalWrite(sigTwo, HIGH);   // sets singal that a pin is disconnected on
-                 
-    
+       
+        //send signal to show that pin is disconnected 
+        if (CellVoltages[CellN] <0.1)
+            {
+                    digitalWrite(sigTwo, HIGH);    // signals that a cvm probe is disconnected 
                      }
-                if ((0.1< CellVoltages[CellN]) && (CellVoltages[CellN] <0.5)) //purge signal
+                     
+        //purge signal             
+        if ((CellVoltages[CellN] > 0.1) && (CellVoltages[CellN] <0.5)) //purge signal
                   {
                      digitalWrite(sigOne, HIGH);   // sends signal to FCC 
                      delay(1500); // waits half a second
@@ -79,12 +80,12 @@ void collectData(void) {
   
         ack = 0;
         adc_code = 0;
- //       ack |= LTC2309_read(chipAddress[chip], adc_command, &adc_code);   // Throws out last reading
-//        ack |= LTC2309_read(chipAddress[chip], adc_command, &adc_code);   // Obtains the current reading and stores to adc_code variable
+         ack |= LTC2309_read(chipAddress[chip], adc_command, &adc_code);   // Throws out last reading
+         ack |= LTC2309_read(chipAddress[chip], adc_command, &adc_code);   // Obtains the current reading and stores to adc_code variable
     
  
         // Convert adc_code to voltage
-        uint8_t adc_voltage = LTC2309_code_to_voltage(adc_code, 2.5f, LTC2309_UNIPOLAR_MODE); //i think the interal reference voltage is 2.5
+        uint8_t adc_voltage = LTC2309_code_to_voltage(adc_code, 4.5f, LTC2309_UNIPOLAR_MODE); //accuracy changes as you change internal ref... maybe try and change it if readings are off
 
 
         //calculating the average of the 20 voltages read on a channel
@@ -109,8 +110,9 @@ void writeData(void) {
   
 for (int out = 0; out<46; out ++){
   Serial.print( CellVoltages[out] );
+  delay (500);
   Serial.print( "," );// add comma, all one line no space?
-  delay(1000);
+  delay(500);
   } 
 }
 
