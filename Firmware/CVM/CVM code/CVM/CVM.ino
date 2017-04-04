1a8f30e54137e140d6d6418de8807c5137ad4eea
egierl <egierl@ualberta.ca>
2017-04-03 22:08:14 -0600

    working test code, shows values of each channel based on colour of the wire for ease of testing

--- Firmware/CVM/CVM code/CVM/CVM.ino
+++ Firmware/CVM/CVM code/CVM/CVM.ino
@@ -9,7 +9,7 @@ int sigOne = A3;                // sig 1 connected to FCC ethernet pin 5
 int sigTwo = A2;                // sig 1 connected to FCC ethernet pin 6
 
 //Variable for storing voltage readings
-float CellVoltages[46];
+float CellVoltages[48];
 // increment variable
 int CellN = 0;
 // holds average voltage value
@@ -21,7 +21,7 @@ uint8_t adc_command =0;
 float adc_voltage= 0;
 
 uint16_t Channels[8] = {0x80,0xC0,0x90,0xD0,0xA0,0xE0,0xB0,0xF0}; //confirm int not char***
-uint16_t chipAddress[6] = {LTC2309_I2C_ADDRESS_1,LTC2309_I2C_ADDRESS_2,LTC2309_I2C_ADDRESS_3,LTC2309_I2C_ADDRESS_4,LTC2309_I2C_ADDRESS_5,LTC2309_I2C_ADDRESS_6}; 
+uint16_t chipAddress[6] = {0x08,0x09,0x0A,0x0B,0x18,0x19}; 
 float arrayAdcVoltage[20]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
 
@@ -33,16 +33,15 @@ pinMode(sigTwo, OUTPUT); // sets the digital pin as output
 digitalWrite(sigOne, LOW);  // turn off signal
 digitalWrite(sigTwo, LOW);  // turn off signal
 }
-//char p = 0;
 
 void loop() 
 {  
 collectData(); //call function collectData
 
   //Calculate voltage of each cell relative to the one before it
-  for(char CellN=45;CellN>0;CellN--)
+ for(char CellN=45;CellN>0;CellN--)
    {
-   CellVoltages[CellN] = CellVoltages[CellN] - CellVoltages[CellN-1];
+  // CellVoltages[CellN] = CellVoltages[CellN] - CellVoltages[CellN-1];
        
         //send signal to show that pin is disconnected 
         if (CellVoltages[CellN] <0.1)
@@ -60,59 +59,62 @@ collectData(); //call function collectData
 
                    }
    }
-                   
+   
+
  writeData();  //call function writeData
 
 }  
 
 
 
-void collectData(void) {
-  for (int chip=0; chip< 6; chip++) {
-  
+void collectData(void) 
+{
+  CellN = 0;
+  for (int chip=0; chip< 6; chip++) 
+  {
+    //Serial.print("Chip ");
+   // Serial.println(chip);
 
-    for (int Channel_number = 0; Channel_number < 8; Channel_number++) {
+    for (int Channel_number = 0; Channel_number < 8; Channel_number++) 
+    {
+        AverageAdcVoltage = 0;
+        for (int N = 0; N<20; N ++)
+        {
     
-        for (int N = 0; N<20; N ++){
-    
-        //Read Channel # in Single-Ended Unipolar mode
-        uint8_t adc_command = Channels[ Channel_number] | LTC2309_UNIPOLAR_MODE;           // Set to read channel #
-  
-        ack = 0;
-        adc_code = 0;
-         ack |= LTC2309_read(chipAddress[chip], adc_command, &adc_code);   // Throws out last reading
-         ack |= LTC2309_read(chipAddress[chip], adc_command, &adc_code);   // Obtains the current reading and stores to adc_code variable
+            //Read Channel # in Single-Ended Unipolar mode
+            adc_command = Channels[ Channel_number] | LTC2309_UNIPOLAR_MODE;           // Set to read channel #
+            ack |= LTC2309_read(chipAddress[chip], adc_command, &adc_code);   // Throws out last reading
+            delay(50);
+            ack |= LTC2309_read(chipAddress[chip], adc_command, &adc_code);   // Obtains the current reading and stores to adc_code variable
     
  
-        // Convert adc_code to voltage
-        uint8_t adc_voltage = LTC2309_code_to_voltage(adc_code, 4.5f, LTC2309_UNIPOLAR_MODE); //accuracy changes as you change internal ref... maybe try and change it if readings are off
+            // Convert adc_code to voltage
+            uint8_t adc_voltage = LTC2309_code_to_voltage(adc_code, 2.5f, LTC2309_UNIPOLAR_MODE); //accuracy changes as you change internal ref... maybe try and change it if readings are off
 
 
-        //calculating the average of the 20 voltages read on a channel
-      
-               while((millis()%100)!= 0); //delay 
+            //calculating the average of the 20 voltages read on a channel
+            AverageAdcVoltage = (AverageAdcVoltage + adc_voltage);
                
-               AverageAdcVoltage = (AverageAdcVoltage + adc_voltage);
-               
-              }
-
-              
-               CellVoltages[CellN] = (AverageAdcVoltage)/20;
-               CellN =(CellN + 1);
-        }
+          }
        
-    
+          CellVoltages[CellN] = (AverageAdcVoltage)/20;
+          CellN =(CellN + 1);
+
+        }
+   
+       
       }
-}
+      
+    }
 
 
 void writeData(void) {
-  
+ 
 for (int out = 0; out<46; out ++){
   Serial.print( CellVoltages[out] );
-  delay (500);
+  //delay (100);
   Serial.print( "," );// add comma, all one line no space?
-  delay(500);
+  delay(250);
   } 
 }
 
--- Firmware/CVM/CVM code/Test code/testI2Ccode/testI2Ccode.ino
+++ Firmware/CVM/CVM code/Test code/testI2Ccode/testI2Ccode.ino
@@ -7,19 +7,20 @@ int sigOne = A3;
 int sigTwo = A2;         
 
 //Variable for storing voltage readings
-float CellVoltages = LTC2309_CH0;
-// increment variable
-int CellN = 0;
+float CellVoltages[48];
+
+
 
 uint8_t ack = 0;
 uint16_t adc_code = 0;
 uint8_t adc_command =0;
 float adc_voltage= 0;
+float AverageAdcVoltage = 0;
 
 
-//int Channels[8] = {0x80,0xC0,0x90,0xD0,0xA0,0xE0,0xB0,0xF0}; //confirm int not char***
+int Channels[8] = {0x80,0xC0,0x90,0xD0,0xA0,0xE0,0xB0,0xF0}; //confirm int not char***
 //uint16_t chipAddress[6] = {LTC2309_I2C_ADDRESS_1,LTC2309_I2C_ADDRESS_2,LTC2309_I2C_ADDRESS_3,LTC2309_I2C_ADDRESS_4,LTC2309_I2C_ADDRESS_5,LTC2309_I2C_ADDRESS_6}; 
-
+uint16_t chipAddress[6] = {0x08,0x09,0x0A,0x0B,0x18,0x19}; 
 void setup() {
 Serial.begin(9600); 
 
@@ -28,61 +29,104 @@ pinMode(sigOne, OUTPUT);  // sets the digital pin as output
 pinMode(sigTwo, OUTPUT); // sets the digital pin as output
 
 digitalWrite(sigOne, LOW);
- digitalWrite(sigTwo, LOW);
+digitalWrite(sigTwo, LOW);
 }
  
 void loop() 
 {
  collectData(); //call function collectData
 
-if(CellVoltages < 0.1 ) 
-         {
-          digitalWrite(sigTwo, HIGH);   // signals that a cvm probe is disconnected 
-             }
-
-
-if ((CellVoltages > 0.1) && (CellVoltages < 0.5)) //purge signal
-    {
-           digitalWrite(sigOne, HIGH);  // sends signal to FCC 
-           delay (2000);   
-           digitalWrite(sigOne, LOW);// turn off signal 
-           delay (2000);   
-           }
-           //signals working!!
-
+//if(CellVoltages[] < 0.1 ) 
+//         {
+//          digitalWrite(sigTwo, HIGH);   // signals that a cvm probe is disconnected 
+//             }
+//
+//
+//if ((CellVoltages > 0.1) && (CellVoltages < 0.5)) //purge signal
+//    {
+//           digitalWrite(sigOne, HIGH);  // sends signal to FCC 
+//           delay (2000);   
+//           digitalWrite(sigOne, LOW);// turn off signal 
+//           delay (2000);   
+//           }
+//           //signals working!!
+//
 
 
 writeData();  //call function writeData
 
 }
                   
-                  
-
-
 
 void collectData(void) {
 
-    adc_command = LTC2309_CH0 | LTC2309_UNIPOLAR_MODE;                  // Build ADC command for channel 0
+    for (int chip=0; chip< 6; chip++) 
+  {
+  int CellN = 0;
+ 
+  for (int Channel_number = 0; Channel_number < 8; Channel_number++) 
+    {
+          
+        for (int N = 0; N<20; N ++)
+        {
+          
+            AverageAdcVoltage = 0;
+    
+            //Read Channel # in Single-Ended Unipolar mode
+            adc_command = Channels[Channel_number] | LTC2309_UNIPOLAR_MODE;        // Set to read channel #
+       
+           ack |= LTC2309_read(chipAddress[chip], adc_command, &adc_code);   // Throws out last reading
+           ack |= LTC2309_read(chipAddress[chip], adc_command, &adc_code);   // Obtains the current reading and stores to adc_code variable
+    
+ 
+            // Convert adc_code to voltage
+            adc_voltage = LTC2309_code_to_voltage(adc_code, 2.5f, LTC2309_UNIPOLAR_MODE); //accuracy changes as you change internal ref... maybe try and change it if readings are off
+            adc_voltage = (adc_voltage);
+           
+            
 
-    ack |= LTC2309_read(LTC2309_I2C_ADDRESS_1, adc_command, &adc_code);   // Throws out last reading
-    delay(50);
-    ack |= LTC2309_read(LTC2309_I2C_ADDRESS_1, adc_command, &adc_code);   // Obtains the current reading and stores to adc_code variable
-
-    // Convert adc_code to voltage
-    adc_voltage = LTC2309_code_to_voltage(adc_code,  4.5f , LTC2309_UNIPOLAR_MODE ); //i think the interal reference voltage is 2.5    
-    CellVoltages = adc_voltage;   
-
-
+            //calculating the average of the 20 voltages read on a channel
+            AverageAdcVoltage = (AverageAdcVoltage + adc_voltage);
+          
+           
+          }
+          
+           CellVoltages[CellN] = (AverageAdcVoltage)/20;
+          CellN =(CellN + 1);
+         
+          
+    }
+  }
+     
 }
 
 
-void writeData(void) { //works!!!!
-  Serial.print( CellVoltages );
-  delay (500);
-  Serial.print( "," );// add comma, all one line no space
-  delay(500);
-}
+void writeData(void) { 
 
+ 
+  for(int count = 0; count < 46; count++ )  { 
+    
+         for (int Read = 0;  Read< 6; Read++ )   
+          {
+             Serial.print( "Chip Number" );
+              Serial.print( Read );
+              Serial.print( ':' ); 
+              delay (600);
+              
+                    for (int values= 0; values < 8; values++)
+                       {
+                         Serial.print( "Chan # " );
+                         Serial.print( values );
+                         Serial.print( " : " );
+                         delay (600);
+                         Serial.print( CellVoltages[count] ); 
+                         Serial.print( ',' ); 
+                         delay (600);
+                        }
+   
+    }
+  }
+}
 
 
 
