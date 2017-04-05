2ae26f5e6bb2c89f644bdb3231412fb65f1cf995
egierl <egierl@ualberta.ca>
2017-04-04 22:41:33 -0600

    more polished cvm code and test code both in theory finshed

--- Firmware/CVM/CVM code/CVM/CVM.ino
+++ Firmware/CVM/CVM code/CVM/CVM.ino
@@ -19,6 +19,9 @@ uint16_t adc_code = 0;
 uint8_t adc_command =0;
 float adc_voltage= 0;
 
+//conversion factor from voltage dividers
+float conversion[48]= {1,1,1,1,1.221,1.47,1.715,2,2.2121,2.4684,2.6949,2.9569,3.2075,3.4271,3.6737, 3.9411,4.1645,4.4013,4.6496,4.9215,5.1667,5.4248,5.6512,5.8780,6.2356,6.4945,6.6179,6.917,7.1728,7.3291,7.6667,7.9930,8.1429,8.5188,8.6923,8.8740,9.0645,9.333,9.6957,9.8496,10.0909,10.3458,10.5238,10.8035,11,11.4932,0,0};
+
 uint16_t Channels[8] = {0x80,0xC0,0x90,0xD0,0xA0,0xE0,0xB0,0xF0}; 
 uint16_t chipAddress[6] = {0x08,0x09,0x0A,0x0B,0x18,0x19}; 
 float arrayAdcVoltage[20]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
@@ -27,7 +30,7 @@ float arrayAdcVoltage[20]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 void setup() {
 Serial.begin(9600); 
 Wire.begin(); 
-pinMode(sigOne, OUTPUT);              // sets the digital pin as output
+pinMode(sigOne, OUTPUT); // sets the digital pin as output
 pinMode(sigTwo, OUTPUT); // sets the digital pin as output
 digitalWrite(sigOne, LOW);  // turn off signal
 digitalWrite(sigTwo, LOW);  // turn off signal
@@ -52,9 +55,9 @@ collectData(); //call function collectData
         if ((CellVoltages[CellNumb] > 0.1) && (CellVoltages[CellNumb] <0.5)) //purge signal
                   {
                      digitalWrite(sigOne, HIGH);   // sends signal to FCC 
-                     delay(1500); // waits half a second
+                     delay(500); // waits half a second
                      digitalWrite(sigOne, LOW);   // sends signal to FCC 
-                     delay(1500);
+                     delay(500);
 
                    }
    }
@@ -95,7 +98,8 @@ void collectData(void)
                
           }
        
-          CellVoltages[CellN] = (AverageAdcVoltage)/20;
+          
+          CellVoltages[CellN] = (((AverageAdcVoltage)/20) *conversion[CellN]);
           CellN =(CellN + 1);
 
         }
@@ -107,13 +111,57 @@ void collectData(void)
 
 
 void writeData(void) {
- 
-for (int out = 0; out<46; out ++){
-  Serial.print( CellVoltages[out] );
-  //delay (100);
-  Serial.print( "," );// add comma, all one line no space?
-  delay(250);
-  } 
+
+    for (int out = 0; out<8; out ++){
+      Serial.print ("0x08: ");
+      Serial.print (out);
+      Serial.print (":");
+      Serial.print(CellVoltages[out]);
+      Serial.print( ',' ); 
+      delay (50);
+      } 
+    for (int out = 8; out<16; out ++){
+      Serial.print ("0x09: ");
+      Serial.print (out);
+      Serial.print (":");
+      Serial.print(CellVoltages[out]);
+      Serial.print( ',' ); 
+      delay (50);
+    
+    for (int out = 16; out<24; out ++){
+      Serial.print ("0x0A: ");
+      Serial.print (out);
+      Serial.print (":");
+      Serial.print(CellVoltages[out]);
+      Serial.print( ',' ); 
+      delay (50);
+    
+    for (int out = 24; out<32; out ++){
+      Serial.print ("0x0B: ");
+      Serial.print (out);
+      Serial.print (":"); 
+      Serial.print(CellVoltages[out]);
+      Serial.print( ',' ); 
+      delay (600);
+    
+    for (int out = 32; out< 40; out ++){
+      Serial.print ("0x18: ");
+      Serial.print (out);
+      Serial.print (":"); 
+      Serial.print(CellVoltages[out]);
+      Serial.print( ',' ); 
+      delay (600);
+    }
+    
+    
+    for (int out = 40 ; out<48; out ++){
+      Serial.print ("0x19: ");
+      Serial.print (out);
+      Serial.print (":");
+      Serial.print(CellVoltages[out]);
+      Serial.print( ',' ); 
+      delay (600);
+  
 }
 
   
